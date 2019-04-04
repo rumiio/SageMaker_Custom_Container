091fc5cc09922cd6ed757134a824f6f11425f160
Rumi <32401858+rumiio@users.noreply.github.com>
2019-04-04 14:29:01 -0700

    Initial commit

---
+++ lambda_function.py
@@ -0,0 +1,32 @@
+import os
+
+import boto3
+import json
+
+# grab environment variables
+ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
+runtime= boto3.client('runtime.sagemaker')
+s3 = boto3.resource('s3')
+
+def lambda_handler(event, context):
+    
+    BUCKET_NAME = 'sagemaker-us-west-2-601091450883' 
+    OBJECT_KEY = 'data/paper.jpg' #plastic-bottle.jpg' test_glass_bottle.jpg'
+    file_name = '/tmp/glass_bottle.jpg'
+    s3.Bucket(BUCKET_NAME).download_file(OBJECT_KEY, '/tmp/glass_bottle.jpg')
+    
+    payload = ''
+
+    with open(file_name, 'rb') as f:
+        payload = f.read()
+        payload = bytearray(payload)
+    
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+                                       ContentType='application/x-image',
+                                       Body=payload)
+
+    result = json.loads(response['Body'].read().decode())
+    print(result)
+    pred = result['predictions']['class']
+    
+    return pred 
