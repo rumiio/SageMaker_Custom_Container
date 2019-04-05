5b1e78621f23088e94d311e5284e40200b6a67ae
rumi <rumi@amazon.com>
2019-04-05 11:56:31 -0700

    debugging

--- build_and_push.sh
+++ build_and_push.sh
@@ -13,7 +13,7 @@ then
     exit 1
 fi
 
-chmod +x decision_trees/serve
+chmod +x image_classification/serve
 
 # Get the account number associated with the current IAM credentials
 account=$(aws sts get-caller-identity --query Account --output text)
